3eae52ff6b6d328ab552497a9ef87532d2a17abd
Andrei Dobrinski <andreiidobrinski@gmail.com>
2021-01-14 18:39:25 -0500

    add random password generator

--- password-generator/src/app/app.component.ts
+++ password-generator/src/app/app.component.ts
@@ -33,6 +33,29 @@ export class AppComponent {
   }
 
   onButtonClick() {
-    this.password = 'asdf';
+    const numbers = '1234567890';
+    const letters = 'abcdefghijklmnopqrstuvwxyz';
+    const symbols = '!@#$%^&*()';
+
+    let validChars = '';
+
+    if (this.includeLetters) {
+      validChars += letters;
+    }
+    if (this.includeNumbers) {
+      validChars += numbers;
+    }
+    if (this.includeSymbols) {
+      validChars += symbols;
+    }
+
+    let generatedPassword = '';
+
+    for (let i = 0; i < this.length; i++) {
+      const index = Math.floor(Math.random() * validChars.length);
+      generatedPassword += validChars[index];
+    }
+
+    this.password = generatedPassword;
   }
 }
