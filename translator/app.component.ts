4883446f324fb913d86bcc6e80311fd1dd09e322
Andrei Dobrinski <andreiidobrinski@gmail.com>
2021-01-13 21:10:28 -0500

    initial app component boilerplate

---
+++ translator/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+import { TranslateService } from "./translate.service";
+
+@Component({
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
+})
+export class AppComponent {
+  constructor(private translateService: TranslateService) {}
+}
