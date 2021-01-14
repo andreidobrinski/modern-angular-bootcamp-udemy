d20ef3fdcf646ad7d95eece94cc1d667c2d4cab0
Andrei Dobrinski <andreiidobrinski@gmail.com>
2021-01-13 21:11:52 -0500

    add submit fn

--- translator/app.component.ts
+++ translator/app.component.ts
@@ -7,5 +7,16 @@ import { TranslateService } from "./translate.service";
   styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
+  text: string;
+  translatedText: string;
+
   constructor(private translateService: TranslateService) {}
+
+  submit() {
+    this.translateService
+      .translate(this.text)
+      .subscribe((result) => {
+        this.translatedText = result;
+      });
+  }
 }
