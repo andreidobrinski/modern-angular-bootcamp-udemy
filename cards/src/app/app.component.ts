199eba310c87aca73407dd2f0527aba9bae11e28
Andrei Dobrinski <andreiidobrinski@gmail.com>
2021-01-15 14:52:36 -0500

    update card template to use passed props

--- cards/src/app/app.component.ts
+++ cards/src/app/app.component.ts
@@ -10,19 +10,19 @@ export class AppComponent {
     {
       title: 'Neat Tree',
       imageUrl: 'assets/tree.jpeg',
-      username: '@nature',
+      username: 'nature',
       content: 'I saw this neat tree today'
     },
     {
       title: 'Snowy Mountain',
       imageUrl: 'assets/mountain.jpeg',
-      username: '@mountain',
+      username: 'mountain',
       content: 'Here is a picture of a snowy mountain'
     },
     {
       title: 'Mountain Biking',
       imageUrl: 'assets/biking.jpeg',
-      username: '@biking',
+      username: 'biking',
       content: 'I did some biking today'
     }
   ];
