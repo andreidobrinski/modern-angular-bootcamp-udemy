79425e52052d2edf960cd6b8ccb6db2415f56a49
Andrei Dobrinski <andreiidobrinski@gmail.com>
2021-01-16 20:27:08 -0500

    show success message if random text = entered text

--- typing-challenge/src/app/app.component.ts
+++ typing-challenge/src/app/app.component.ts
@@ -8,4 +8,9 @@ import { lorem } from 'faker';
 })
 export class AppComponent {
   randomText = lorem.sentence();
+  enteredText = '';
+
+  onInput(value: string) {
+    this.enteredText = value;
+  }
 }
