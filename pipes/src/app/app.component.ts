73e7bfa3d031fcae6ee697c2f9f90c64f7408806
Andrei Dobrinski <andreiidobrinski@gmail.com>
2021-01-16 21:35:57 -0500

    add pipe for number format

--- pipes/src/app/app.component.ts
+++ pipes/src/app/app.component.ts
@@ -9,6 +9,7 @@ export class AppComponent {
   name: string;
   date: string;
   amount: number;
+  height: number;
 
   onNameChange(value: string) {
     this.name = value;
@@ -20,5 +21,9 @@ export class AppComponent {
 
   onAmountChange(value: string) {
     this.amount = parseFloat(value);
+  }
+
+  onHeightChange(value: string) {
+    this.height = parseFloat(value);
   }
 }
