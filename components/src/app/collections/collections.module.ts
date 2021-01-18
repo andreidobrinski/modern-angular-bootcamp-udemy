5a40194274e02569f51614e289c414605eb784f2
Andrei Dobrinski <andreiidobrinski@gmail.com>
2021-01-18 17:45:20 -0500

    add routing for collections component

--- components/src/app/collections/collections.module.ts
+++ components/src/app/collections/collections.module.ts
@@ -11,6 +11,5 @@ import { CollectionsHomeComponent } from './collections-home/collections-home.co
     CommonModule,
     CollectionsRoutingModule
   ],
-  exports: [CollectionsHomeComponent]
 })
 export class CollectionsModule { }
