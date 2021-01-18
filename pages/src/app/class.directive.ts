e048a30002d505b23e4403e3b6764ae4231d06f6
Andrei Dobrinski <andreiidobrinski@gmail.com>
2021-01-18 13:51:18 -0500

    replace ngClass with custom appClass directive

--- pages/src/app/class.directive.ts
+++ pages/src/app/class.directive.ts
@@ -9,4 +9,14 @@ export class ClassDirective {
   @Input('appClass') set backgroundColor(color: string) {
     this.element.nativeElement.style.backgroundColor = color;
   }
+
+  @Input('appClass') set classNames(classObj: any) {
+    for (let key in classObj) {
+      if (classObj[key]) {
+        this.element.nativeElement.classList.add(key);
+      } else {
+        this.element.nativeElement.classList.remove(key);
+      }
+    }
+  }
 }
