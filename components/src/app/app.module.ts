eaad9b78ea5fcc9619b8e90181f347af7f7cd085
Andrei Dobrinski <andreiidobrinski@gmail.com>
2021-01-19 14:38:27 -0500

    add lazyload for elements module

--- components/src/app/app.module.ts
+++ components/src/app/app.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ElementsModule } from './elements/elements.module';
 import { CollectionsModule } from './collections/collections.module';
 import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './not-found/not-found.component';
@@ -16,7 +15,6 @@ import { NotFoundComponent } from './not-found/not-found.component';
   ],
   imports: [
     BrowserModule,
-    ElementsModule,
     CollectionsModule,
     AppRoutingModule,
   ],
