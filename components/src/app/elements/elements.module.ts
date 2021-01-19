bc3353f5ad73a16130c48baeb0ed7c6f10638f7d
Andrei Dobrinski <andreiidobrinski@gmail.com>
2021-01-19 18:26:46 -0500

    add divider widget with ng-content

--- components/src/app/elements/elements.module.ts
+++ components/src/app/elements/elements.module.ts
@@ -5,13 +5,14 @@ import { ElementsRoutingModule } from './elements-routing.module';
 import { ElementsHomeComponent } from './elements-home/elements-home.component';
 import { PlaceholderComponent } from './placeholder/placeholder.component';
 import { TimesDirective } from './times.directive';
-
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [ElementsHomeComponent, PlaceholderComponent, TimesDirective],
   imports: [
     CommonModule,
-    ElementsRoutingModule
+    ElementsRoutingModule,
+    SharedModule,
   ],
   exports: []
 })
