cbaf2351b766f0d058ebfb7ed78366a78e93afb7
Andrei Dobrinski <andreiidobrinski@gmail.com>
2021-01-19 18:56:33 -0500

    add reusable table

--- components/src/app/collections/collections-home/collections-home.component.ts
+++ components/src/app/collections/collections-home/collections-home.component.ts
@@ -6,6 +6,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./collections-home.component.css']
 })
 export class CollectionsHomeComponent implements OnInit {
+  data = [
+    {
+      name: 'name',
+      age: 41,
+      job: 'job'
+    },
+    {
+      name: 'name2',
+      age: 43,
+      job: 'job2'
+    },
+    {
+      name: 'nam3',
+      age: 43,
+      job: 'job3'
+    },
+  ];
+  headers = [
+    { key: 'name', label: 'Name' },
+    { key: 'age', label: 'Age' },
+    { key: 'job', label: 'Job' },
+  ];
 
   constructor() { }
 
