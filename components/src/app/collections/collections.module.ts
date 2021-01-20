54d4b96c6f11f4a24f73679a3ad6c57e43348b15
Andrei Dobrinski <andreiidobrinski@gmail.com>
2021-01-19 19:14:48 -0500

    add child routing for collections home

--- components/src/app/collections/collections.module.ts
+++ components/src/app/collections/collections.module.ts
@@ -5,9 +5,12 @@ import { CollectionsRoutingModule } from './collections-routing.module';
 import { CollectionsHomeComponent } from './collections-home/collections-home.component';
 import { TableComponent } from './table/table.component';
 import { SharedModule } from '../shared/shared.module';
+import { BiographyComponent } from './biography/biography.component';
+import { CompaniesComponent } from './companies/companies.component';
+import { PartnersComponent } from './partners/partners.component';
 
 @NgModule({
-  declarations: [CollectionsHomeComponent, TableComponent],
+  declarations: [CollectionsHomeComponent, TableComponent, BiographyComponent, CompaniesComponent, PartnersComponent],
   imports: [
     CommonModule,
     CollectionsRoutingModule,
