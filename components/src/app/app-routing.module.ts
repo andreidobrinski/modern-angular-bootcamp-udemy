7b992ba372a7916f61cb06eb2811b648b6aaf907
Andrei Dobrinski <andreiidobrinski@gmail.com>
2021-01-20 17:37:00 -0500

    add new mods tab

--- components/src/app/app-routing.module.ts
+++ components/src/app/app-routing.module.ts
@@ -22,6 +22,12 @@ const routes: Routes = [
       import('./views/views.module')
       .then(m => m.ViewsModule)
   },
+  {
+    path: 'mods',
+    loadChildren: () =>
+      import('./mods/mods.module')
+      .then(m => m.ModsModule)
+  },
   { path: '', component: HomeComponent },
   { path: '**', component: NotFoundComponent }
 ];
