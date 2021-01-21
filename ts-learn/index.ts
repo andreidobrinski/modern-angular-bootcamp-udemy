68e6b8c2a85f0f26a27e15172b28818c3892d21a
Andrei Dobrinski <andreiidobrinski@gmail.com>
2021-01-21 14:47:15 -0500

    type inference notes

--- ts-learn/index.ts
+++ ts-learn/index.ts
@@ -8,3 +8,11 @@ const yes: boolean = true;
 
 const nothingHere: null = null;
 const noDef: undefined = undefined;
+
+let myName2 = 'andrei';
+// TS will throw warning b/c of type inference
+// myName2 = 10;
+// no need to annotate myName2
+
+// useful to annotate types if not initializing them
+let myName3: string;
