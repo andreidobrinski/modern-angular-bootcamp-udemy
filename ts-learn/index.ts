8b286955c18ddd04b568803d94d54e5876b037f7
Andrei Dobrinski <andreiidobrinski@gmail.com>
2021-01-21 14:43:27 -0500

    ts: basic types

---
+++ ts-learn/index.ts
@@ -0,0 +1,10 @@
+const myName: string = 'Andrei';
+
+const ten:number = 10;
+const negative: number = -10;
+const decimal: number = 10.1;
+
+const yes: boolean = true;
+
+const nothingHere: null = null;
+const noDef: undefined = undefined;
