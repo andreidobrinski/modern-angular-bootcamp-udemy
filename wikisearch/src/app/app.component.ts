007bd3b9c4b19e4d2fb240104e72bd51da436eab
Andrei Dobrinski <andreiidobrinski@gmail.com>
2021-01-22 14:55:34 -0500

    pass search term to wikipedia service

--- wikisearch/src/app/app.component.ts
+++ wikisearch/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { WikipediaService } from './wikipedia.service';
 
 @Component({
   selector: 'app-root',
@@ -6,7 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  // creates wikipedia var as an instance of WikipediaService
+  constructor(private wikipedia: WikipediaService) {}
+
   onTerm(term: string) {
-    console.log(term)
+    const results = this.wikipedia.search(term);
   }
 }
--- wikisearch/src/app/wikipedia.service.ts
+++ wikisearch/src/app/wikipedia.service.ts
@@ -4,6 +4,9 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class WikipediaService {
-
   constructor() { }
+
+  public search(term: string) {
+
+  }
 }
