6cd6ef74267d4aec87ea121478d3a4a6b98c28e0
Andrei Dobrinski <andreiidobrinski@gmail.com>
2021-01-24 17:44:06 -0500

    add get call with api key

--- photos/src/app/photos.service.ts
+++ photos/src/app/photos.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { apiKey } from './api-key';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PhotosService {
   constructor(private http: HttpClient) {}
+
+  getPhoto() {
+    return this.http.get('https://api.unsplash.com/photos/random', {
+    headers: {
+      Authorization: `Client-ID ${apiKey}`
+    }
+    });
+  }
 }
