95e4fcf2d6495ebbc6404e7b59671e64fa80c7d4
Andrei Dobrinski <andreiidobrinski@gmail.com>
2021-01-25 09:53:22 -0500

    refactor input to separate component

--- creditcard/src/app/app.module.ts
+++ creditcard/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { CardFormComponent } from './card-form/card-form.component';
+import { InputComponent } from './input/input.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CardFormComponent
+    CardFormComponent,
+    InputComponent
   ],
   imports: [
     BrowserModule,
