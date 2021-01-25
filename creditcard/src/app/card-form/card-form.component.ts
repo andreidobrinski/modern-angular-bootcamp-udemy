46f6f5c3a7777554eeff9fd21ad34f596582ba2e
Andrei Dobrinski <andreiidobrinski@gmail.com>
2021-01-25 17:08:29 -0500

    handle form reset

--- creditcard/src/app/card-form/card-form.component.ts
+++ creditcard/src/app/card-form/card-form.component.ts
@@ -37,4 +37,8 @@ export class CardFormComponent implements OnInit {
   onSubmit() {
     console.log('form submitted')
   }
+
+  onResetClick() {
+    this.cardForm.reset();
+  }
 }
--- creditcard/src/app/date-form-control.ts
+++ creditcard/src/app/date-form-control.ts
@@ -1,7 +1,16 @@
 import { FormControl } from '@angular/forms';
 
 export class DateFormControl extends FormControl {
-  setValue(value: string, options: any) {
+  setValue(value: string | null, options: any) {
+    // handle reset case
+    if (!value) {
+      super.setValue(
+        '',
+        { ...options, emitModelToViewChange: true }
+      );
+      return;
+    }
+
     // if user inputs anything that isnt a number, don't allow it
     if (value.match(/[^0-9|\/]/gi)) {
       super.setValue(
