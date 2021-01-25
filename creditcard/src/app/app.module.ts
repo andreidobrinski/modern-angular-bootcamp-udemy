9df390dbea23ad1fa9e5297eba92242e5427bb05
Andrei Dobrinski <andreiidobrinski@gmail.com>
2021-01-25 17:18:28 -0500

    add style for credit card live update

--- creditcard/src/app/app.module.ts
+++ creditcard/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { CardFormComponent } from './card-form/card-form.component';
 import { InputComponent } from './input/input.component';
+import { CardComponent } from './card/card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CardFormComponent,
-    InputComponent
+    InputComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
