93b6c18e7107cb5a5a45386efa49af71601cb17c
Andrei Dobrinski <andreiidobrinski@gmail.com>
2021-02-09 14:45:25 -0500

    pass args to validator fn

--- mathform/src/app/math-validators.ts
+++ mathform/src/app/math-validators.ts
@@ -1,12 +1,16 @@
 import { AbstractControl } from "@angular/forms";
 
 export class MathValidators {
-  static addition(form: AbstractControl) {
-    const { a, b, answer } = form.value;
-      if (a + b === parseInt(answer)) {
+  static addition(target: string, sourceOne: string, sourceTwo: string) {
+    return (form: AbstractControl) => {
+      const sum = form.value[target];
+      const firstNumber = form.value[sourceOne];
+      const secondNumber = form.value[sourceTwo];
+      if (firstNumber + secondNumber === parseInt(sum)) {
         return null;
       }
 
       return { addition: true };
+    };
   }
 }
