b3a334a901ec2631b4b86ec8af141c59b278f352
Andrei Dobrinski <andreiidobrinski@gmail.com>
2021-02-12 14:50:42 -0500

    refactor to use auth service

--- emailclient/src/app/auth/validators/unique-username.ts
+++ emailclient/src/app/auth/validators/unique-username.ts
@@ -1,21 +1,17 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 import { AsyncValidator, FormControl } from '@angular/forms';
 import { map, catchError } from 'rxjs/operators';
 import { of } from 'rxjs';
+import { AuthService } from '../auth.service';
 
 @Injectable({ providedIn: 'root' })
 export class UniqueUsername implements AsyncValidator {
-  constructor(private http: HttpClient) {}
+  constructor(private authService: AuthService) {}
 
   validate = (control: FormControl) => {
     const { value } = control;
 
-    return this.http
-      .post<any>(
-        'https://api.angular-email.com/auth/username',
-        { username: value }
-      )
+    return this.authService.usernameAvailable(value)
       .pipe(
         map(value => {
           if (value.available) {
