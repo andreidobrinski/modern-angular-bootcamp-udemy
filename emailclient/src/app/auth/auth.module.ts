fdaebaeee5ec83687d5a6c96412e2bf368ee7e99
Andrei Dobrinski <andreiidobrinski@gmail.com>
2021-02-12 14:57:01 -0500

    start on input component template

--- emailclient/src/app/auth/auth.module.ts
+++ emailclient/src/app/auth/auth.module.ts
@@ -4,6 +4,7 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { AuthRoutingModule } from './auth-routing.module';
 import { SigninComponent } from './signin/signin.component';
 import { SignupComponent } from './signup/signup.component';
+import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule({
@@ -12,6 +13,7 @@ import { SignupComponent } from './signup/signup.component';
     CommonModule,
     AuthRoutingModule,
     ReactiveFormsModule,
+    SharedModule,
   ]
 })
 export class AuthModule { }
