8e0f1a646e50fba9826cb52c31869429a59811c5
Andrei Dobrinski <andreiidobrinski@gmail.com>
2021-02-12 09:19:13 -0500

    add http request to check for unique username

--- emailclient/src/app/auth/validators/unique-username.ts
+++ emailclient/src/app/auth/validators/unique-username.ts
@@ -1,12 +1,34 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { AsyncValidator } from '@angular/forms';
+import { AsyncValidator, FormControl } from '@angular/forms';
+import { map, catchError } from 'rxjs/operators';
+import { of } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class UniqueUsername implements AsyncValidator {
   constructor(private http: HttpClient) {}
 
-  validate() {
-    
+  validate = (control: FormControl) => {
+    const { value } = control;
+
+    return this.http
+      .post<any>(
+        'https://api.angular-email.com/auth/username',
+        { username: value }
+      )
+      .pipe(
+        map(value => {
+          if (value.available) {
+            return null;
+          }
+        }),
+        catchError((err) => {
+          if (err.error.username) {
+            return of({ nonUniqueUsername: true })
+          } else {
+            return of({ noConnection: true })
+          }
+        })
+      )
   }
 }
