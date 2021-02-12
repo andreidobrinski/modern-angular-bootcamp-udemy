43fa1f0a2884d5033670eb1693d34834ac278d81
Andrei Dobrinski <andreiidobrinski@gmail.com>
2021-02-12 08:30:07 -0500

    dependency injection: add match password validator

--- emailclient/src/app/auth/validators/match-password.ts
+++ emailclient/src/app/auth/validators/match-password.ts
@@ -1,5 +1,7 @@
+import { Injectable } from '@angular/core';
 import { FormGroup, Validator } from '@angular/forms';
 
+@Injectable({ providedIn: 'root' }) 
 export class MatchPassword implements Validator {
   validate(formGroup: FormGroup) {
     const { password, passwordConfirmation } = formGroup.value;
