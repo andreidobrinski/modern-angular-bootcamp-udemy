0ab5c354688401f421a448c615e867e853db8ae4
Andrei Dobrinski <andreiidobrinski@gmail.com>
2021-02-16 18:43:08 -0500

    add signout fn

--- emailclient/src/app/auth/auth.service.ts
+++ emailclient/src/app/auth/auth.service.ts
@@ -61,4 +61,15 @@ export class AuthService {
         })
       );
   }
+
+  signout() {
+    return this.http.post(
+        `${this.rootUrl}/auth/signout`,
+        {}
+      ).pipe(
+        tap(() => {
+          this.signedin$.next(false);
+        })
+      );
+  }
 }
