c77984e8184003a0af5d4dc086c5119a460fa51a
Andrei Dobrinski <andreiidobrinski@gmail.com>
2021-03-10 13:24:13 -0500

    complete authguard with tristate

--- emailclient/src/app/auth/auth.guard.ts
+++ emailclient/src/app/auth/auth.guard.ts
@@ -1,18 +1,21 @@
 import { Injectable } from '@angular/core';
 import { CanLoad, Route, UrlSegment, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import { take, skipWhile } from 'rxjs/operators';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanLoad {
+  constructor(private authService: AuthService) {}
+
   canLoad(
     route: Route,
     segments: UrlSegment[]): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-
-    return new Observable((subscriber) => {
-      subscriber.next(true);
-      subscriber.complete();
-    });
+    return this.authService.signedin$.pipe(
+      skipWhile(value => value === null),
+      take(1)
+    );
   }
 }
