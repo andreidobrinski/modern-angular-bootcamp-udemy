36bf1811196e55b75bd03cf6dd3d990f499729ff
Andrei Dobrinski <andreiidobrinski@gmail.com>
2021-03-10 13:27:12 -0500

    navigate to root route if not authenticated

--- emailclient/src/app/auth/auth.guard.ts
+++ emailclient/src/app/auth/auth.guard.ts
@@ -1,21 +1,26 @@
 import { Injectable } from '@angular/core';
-import { CanLoad, Route, UrlSegment, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { CanLoad, Route, UrlSegment, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Resolve, Router } from '@angular/router';
 import { Observable } from 'rxjs';
-import { take, skipWhile } from 'rxjs/operators';
+import { take, skipWhile, tap } from 'rxjs/operators';
 import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanLoad {
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService, private router: Router) {}
 
   canLoad(
     route: Route,
     segments: UrlSegment[]): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
     return this.authService.signedin$.pipe(
       skipWhile(value => value === null),
-      take(1)
+      take(1),
+      tap((authenticated) => {
+        if (!authenticated) {
+          this.router.navigateByUrl('/');
+        }
+      })
     );
   }
 }
