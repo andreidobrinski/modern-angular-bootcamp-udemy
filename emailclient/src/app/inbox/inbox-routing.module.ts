2568790f6d7a853082a14229dc10beece4e62bdb
Andrei Dobrinski <andreiidobrinski@gmail.com>
2021-03-10 16:50:35 -0500

    add error handling with email not found

--- emailclient/src/app/inbox/inbox-routing.module.ts
+++ emailclient/src/app/inbox/inbox-routing.module.ts
@@ -4,12 +4,17 @@ import { EmailShowComponent } from './email-show/email-show.component';
 import { HomeComponent } from './home/home.component';
 import { PlaceholderComponent } from './placeholder/placeholder.component';
 import { EmailResolverService } from './email-resolver.service';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
     children: [
+      {
+        path: 'not-found',
+        component: NotFoundComponent
+      },
       {
         path: ':id',
         component: EmailShowComponent,
