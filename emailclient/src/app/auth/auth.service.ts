cdbfabdbe517f68a14d1d65a7654abff48c35c1f
Andrei Dobrinski <andreiidobrinski@gmail.com>
2021-03-13 18:45:43 -0500

    pull username from auth service

--- emailclient/src/app/auth/auth.service.ts
+++ emailclient/src/app/auth/auth.service.ts
@@ -27,12 +27,17 @@ interface SigninCredentials {
   password: string;
 }
 
+interface SigninResponse {
+  username: string;
+}
+
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   rootUrl = 'https://api.angular-email.com';
   signedin$ = new BehaviorSubject(null);
+  username = '';
 
   constructor(private http: HttpClient) {}
 
@@ -50,8 +55,9 @@ export class AuthService {
         `${this.rootUrl}/auth/signup`,
         credentials
       ).pipe(
-        tap(() => {
+        tap(({ username }) => {
           this.signedin$.next(true);
+          this.username = username;
         })
       );
   }
@@ -61,8 +67,9 @@ export class AuthService {
         `${this.rootUrl}/auth/signedin`
       )
       .pipe(
-        tap(({ authenticated }) => {
+        tap(({ authenticated, username }) => {
           this.signedin$.next(authenticated);
+          this.username = username;
         })
       );
   }
@@ -80,13 +87,14 @@ export class AuthService {
 
   signin(credentials: SigninCredentials) {
     return this.http
-      .post(
+      .post<SigninResponse>(
         `${this.rootUrl}/auth/signin`,
         credentials
       )
       .pipe(
-        tap(() => {
+        tap(({ username }) => {
           this.signedin$.next(true);
+          this.username = username;
         })
       )
   }
