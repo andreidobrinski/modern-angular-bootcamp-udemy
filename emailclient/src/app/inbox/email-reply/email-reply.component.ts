a22211db8a1a3857bd1eefa08be9fe1272e0ee0c
Andrei Dobrinski <andreiidobrinski@gmail.com>
2021-03-14 14:50:00 -0400

    change ngOnInit to ngOnChanges

--- emailclient/src/app/inbox/email-reply/email-reply.component.ts
+++ emailclient/src/app/inbox/email-reply/email-reply.component.ts
@@ -7,13 +7,13 @@ import { EmailService } from '../email.service';
   templateUrl: './email-reply.component.html',
   styleUrls: ['./email-reply.component.css']
 })
-export class EmailReplyComponent implements OnInit {
+export class EmailReplyComponent {
   showModal = false;
   @Input() email: Email;
 
   constructor(private emailService: EmailService) { }
 
-  ngOnInit(): void {
+  ngOnChanges(): void {
     const text = this.email.text.replace(/\n/gi, '\n> ');
 
     this.email = {
