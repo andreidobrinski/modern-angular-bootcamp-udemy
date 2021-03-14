8e2212a944e93b6db5cad5b0f3733e89c3a9d562
Andrei Dobrinski <andreiidobrinski@gmail.com>
2021-03-14 15:11:59 -0400

    add weather module to app module

--- landing/src/app/app.module.ts
+++ landing/src/app/app.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { WeatherModule } from './weather/weather.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    WeatherModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
