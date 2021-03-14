47d55265f30ebda4789a9365b38ffb7497077399
Andrei Dobrinski <andreiidobrinski@gmail.com>
2021-03-14 15:50:19 -0400

    add getForecast with map pipe

--- landing/src/app/weather/forecast.service.ts
+++ landing/src/app/weather/forecast.service.ts
@@ -1,11 +1,27 @@
 import { Injectable } from '@angular/core';
+import { HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { weatherKey } from '../../../apiKey';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ForecastService {
   constructor() { }
+
+  getForecast() {
+    return this.getCurrentLocation()
+      .pipe(
+        map(coords => {
+          return new HttpParams()
+            .set('lat', String(coords.latitude))
+            .set('lon', String(coords.longitude))
+            .set('units', 'metric')
+            .set('appid', weatherKey)
+        })
+      )
+  }
 
   getCurrentLocation() {
     return new Observable<GeolocationCoordinates>((observer) => {
