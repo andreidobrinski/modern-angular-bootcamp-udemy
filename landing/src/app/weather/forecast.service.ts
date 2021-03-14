64dfe100867b89f9eb3357780c5df817cab1205b
Andrei Dobrinski <andreiidobrinski@gmail.com>
2021-03-14 17:02:13 -0400

    return array from forecast observable

--- landing/src/app/weather/forecast.service.ts
+++ landing/src/app/weather/forecast.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpParams, HttpClient } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
-import { map, switchMap, pluck, mergeMap, filter } from 'rxjs/operators';
+import { map, switchMap, pluck, mergeMap, filter, toArray } from 'rxjs/operators';
 import { weatherKey } from '../../../apiKey';
 
 interface OpenWeatherResponse {
@@ -34,7 +34,14 @@ export class ForecastService {
         switchMap(params => this.http.get<OpenWeatherResponse>(this.url, { params })),
         pluck('list'),
         mergeMap(value => of(...value)),
-        filter((value, index) => index % 8 === 0)
+        filter((value, index) => index % 8 === 0),
+        map(value => {
+          return {
+            dateString: value.dt_txt,
+            temp: value.main.temp
+          }
+        }),
+        toArray()
       );
   }
 
