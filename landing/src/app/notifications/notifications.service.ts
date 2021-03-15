3e79b98335177687b58c27e7cc8f8d7e6f4b9808
Andrei Dobrinski <andreiidobrinski@gmail.com>
2021-03-15 13:21:44 -0400

    start on notifications service subject

--- landing/src/app/notifications/notifications.service.ts
+++ landing/src/app/notifications/notifications.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NotificationsService {
+  messages: Subject<any>;
 
-  constructor() { }
+  constructor() {
+    this.messages = new Subject<any>();
+  }
+
+  addSuccess(message: string) {
+    this.messages.next(message);
+  }
+
+  addError(message: string) {
+    this.messages.next(message);
+  }
+
+  clearMessage(id: number) {}
 }
