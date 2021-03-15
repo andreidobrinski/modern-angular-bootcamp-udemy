4147588c5cacdafff0d94d2219ff346630e957c5
Andrei Dobrinski <andreiidobrinski@gmail.com>
2021-03-15 13:26:39 -0400

    update subject with type interface

--- landing/src/app/notifications/notifications.service.ts
+++ landing/src/app/notifications/notifications.service.ts
@@ -1,23 +1,46 @@
 import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
 
+interface Command {
+  id: number;
+  type: 'success' | 'error' | 'clear';
+  text?: string;
+}
+
 @Injectable({
   providedIn: 'root'
 })
 export class NotificationsService {
-  messages: Subject<any>;
+  messages: Subject<Command>;
 
   constructor() {
-    this.messages = new Subject<any>();
+    this.messages = new Subject<Command>();
   }
 
   addSuccess(message: string) {
-    this.messages.next(message);
+    this.messages.next({
+      id: this.randomId(),
+      text: message,
+      type: 'success'
+    });
   }
 
   addError(message: string) {
-    this.messages.next(message);
+    this.messages.next({
+      id: this.randomId(),
+      text: message,
+      type: 'error'
+    });
   }
 
-  clearMessage(id: number) {}
+  clearMessage(id: number) {
+    this.messages.next({
+      id,
+      type: 'clear'
+    });
+  }
+
+  private randomId() {
+    return Math.round(Math.random() * 10000);
+  }
 }
