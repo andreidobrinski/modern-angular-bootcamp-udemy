e1686de8846ad1efbad1aef19d51cb799c4f5b8b
Andrei Dobrinski <andreiidobrinski@gmail.com>
2021-03-15 15:58:32 -0400

    add properties to news api service

--- landing/src/app/news-api/news-api.service.ts
+++ landing/src/app/news-api/news-api.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { newsKey } from '../../../apiKey';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NewsApiService {
+  private url = 'https://newsapi.org/v2/top-headlines';
+  private pageSize = 10;
+  private apiKey = newsKey;
+  private country = 'ca';
 
   constructor() { }
 }
